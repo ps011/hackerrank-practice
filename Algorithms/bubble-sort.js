ac88786329d7653c990d27f7f6c320dcb5550ccb
Prasheel Soni <prasheelsoni11@gmail.com>
2020-09-16 00:46:27 +0530

    add bubble sort

---
+++ Algorithms/bubble-sort.js
@@ -0,0 +1,18 @@
+const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
+
+function bubbleSort (array) {
+    for(let i = 0; i < array.length; i++) {
+        for(let j = 0; j < array.length; j++) {
+            if (array[j] > array[j+1]) {
+                const temp = array[j];
+                array[j] = array[j+1];
+                array[j+1] = temp;
+            }
+        }
+    }
+return array;
+}
+
+
+const answer = bubbleSort(numbers);
+console.log(answer);
