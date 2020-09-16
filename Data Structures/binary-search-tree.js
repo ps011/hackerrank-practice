2a1fe2c52a3f481578c18f79c1d745b69b3bccb7
Prasheel Soni <prasheelsoni11@gmail.com>
2020-09-16 23:58:54 +0530

    add BFS recursive approach

--- Data Structures/binary-search-tree.js
+++ Data Structures/binary-search-tree.js
@@ -146,6 +146,21 @@ class Node {
         }
         return list;
     }
+
+    breadthFirstSearchRecursive(queue, list) {
+        if (!queue.length) {
+            return list;
+        }
+        let currentNode = queue.shift();
+        list.push(currentNode.value);
+        if (currentNode.left) {
+            queue.push(currentNode.left)
+        }
+        if (currentNode.right) {
+            queue.push(currentNode.right)
+        }
+        return this.breadthFirstSearchRecursive(queue, list);
+    }
   }
   
   const tree = new BinarySearchTree();
@@ -157,8 +172,10 @@ class Node {
   tree.insert(15)
   tree.insert(1)
 //   tree.remove(170)
-//   console.log(JSON.stringify(traverse(tree.root)))
-  console.log(tree.breadthFirstSearch());
+  JSON.stringify(traverse(tree.root));
+//   console.log(tree.breadthFirstSearch());
+  console.log(tree.breadthFirstSearchRecursive([tree.root], []))
+
   
   //     9
   //  4     20
