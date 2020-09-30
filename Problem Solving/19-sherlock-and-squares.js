b48c6f857f63114b20f52697eaed4b2e1ec10651
Prasheel Soni <prasheelsoni11@gmail.com>
2020-09-30 19:34:07 +0530

    sherlock and squares

---
+++ Problem Solving/19-sherlock-and-squares.js
@@ -0,0 +1,13 @@
+// Complete the squares function below.
+function squares(a, b) {
+     return Math.floor(Math.sqrt(b)) - Math.ceil(Math.sqrt(a)) + 1;
+}
+
+squares(3,9); // 2
+squares(17,24); // 0
+squares(35,70); // 3
+squares(100,1000); // 22
+
+
+// 14 15 16 17 18 19 20 21 22 23 24 25
+// 3.74                             5
